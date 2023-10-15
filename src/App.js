5dc59724107fb7908dec0ad2fb63f7ea3c3a611b
Raaj1500s <mr.k.manikanta99@gmail.com>
2023-10-15 11:28:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Table from './components/Table';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Table/>
+    </div>
+    );
+}
+
+export default App;
+
+/*
+echo "# ReactJSTask-4" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Raaj1500s/ReactJSTask-4.git
+git push -u origin main
+*/
